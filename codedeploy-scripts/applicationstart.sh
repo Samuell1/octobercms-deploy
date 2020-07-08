dd54af8785548c362fb7e43ac9414fc67b325d7e
Samuell <Samuell1@users.noreply.github.com>
2020-07-08 15:26:51 +0200

    Update applicationstart.sh

--- codedeploy-scripts/applicationstart.sh
+++ codedeploy-scripts/applicationstart.sh
@@ -6,9 +6,6 @@ echo "Running Hook: applicationstart"
 service php7.3-fpm restart
 service nginx restart
 
-# restart codedeploy-agent to prevent memory leak https://github.com/aws/aws-codedeploy-agent/issues/32
-service codedeploy-agent restart
-
 echo "Finished running hook: applicationstart"
 
 exit 0
